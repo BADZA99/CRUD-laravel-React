5138d7601540138b53368b738445cb1c0487b879
papabn <pndiaye999@gmail.com>
2023-10-21 22:02:18 +0000

    finish

--- react/src/assets/views/Users.jsx
+++ react/src/assets/views/Users.jsx
@@ -3,10 +3,12 @@ import { useEffect } from 'react';
 import { useState } from 'react'
 import axiosClient from '../../axios-client';
 import { Link } from 'react-router-dom';
+import { useStateContext } from '../contexts/ContextProvider';
 
 export default function Users() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
+  const {setNotification}=useStateContext();
 
   useEffect(() => {
     getUsers();
@@ -18,7 +20,7 @@ export default function Users() {
     }
     axiosClient.delete(`/users/${user.id}`)
       .then(() => {
-        // setNotification('User was successfully deleted')
+        setNotification('User was successfully deleted')
         getUsers()
       })
   }
